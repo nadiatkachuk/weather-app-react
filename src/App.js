8b33be9847df45ded992d98fb7dca3cc1c54a335
nadiatkachuk <126887250+nadiatkachuk@users.noreply.github.com>
2023-10-06 04:13:46 +0300

    changed city

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from React</h1>
-        <Weather city="Kyiv" />
+        <Weather city="Lviv" />
       </header>
     </div>
   );
